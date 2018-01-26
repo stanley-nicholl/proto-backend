67e53ffd175e89bf9661c36828bf766ce06934b0
Stanley <snicholl@gmail.com>
2018-01-25 21:16:37 -0800

    small changes

--- db/seeds/03_prototype_reviews.js
+++ db/seeds/03_prototype_reviews.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex, Promise) {
   // Inserts sample users
   return knex('prototype_reviews').insert([
-    { id: 1, prototype_id: 1, user_id: 1, painGain: true, simple: false, changeRequest: 'I like the direction of where this is going. Needs to enable other communication methods beyond text.', nextSteps: 'review with Brian Delmico' },
+    { id: 1, prototype_id: 1, user_id: 2, painGain: true, simple: false, changeRequest: 'I like the direction of where this is going. Needs to enable other communication methods beyond text.', nextSteps: 'review with Brian Delmico' },
     { id: 2, prototype_id: 1, user_id: 3, painGain: true, simple: false, changeRequest: 'I like the direction of where this is going. Needs to enable other communication methods beyond text.', nextSteps: 'review with Brian Delmico'}
   ])
   .then(() => {
--- models/prototypes.model.js
+++ models/prototypes.model.js
@@ -7,12 +7,11 @@ class prototypesModel extends Model{
     return db('prototype_reviews')
     .join('users', 'prototype_reviews.user_id', 'users.id')
     .join('prototypes', 'prototype_reviews.prototype_id', 'prototypes.id')
-    .select('prototype_reviews.id', 'prototypes.id as prototype_id', 'prototypes.name', 'prototype_reviews.painGain', 'prototype_reviews.simple', 'prototype_reviews.changeRequest', 'prototype_reviews.nextSteps', 'users.id as user_id', 'users.first_name', 'users.last_name')
+    .select('prototype_reviews.id', 'prototypes.id as prototype_id', 'prototypes.name', 'prototype_reviews.painGain', 'prototype_reviews.simple', 'prototype_reviews.changeRequest', 'prototype_reviews.nextSteps', 'users.id as user_id', 'users.first_name', 'users.last_name', 'users.image')
     .where({ 'prototype_reviews.prototype_id': id})
   }
 
   static createReview(body) {
-    console.log(body);
     return db('prototype_reviews')
     .insert(body)
     .returning('*')
