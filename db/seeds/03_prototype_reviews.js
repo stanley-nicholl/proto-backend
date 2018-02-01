1f2090f018f89b2b3a9fac4f9b3fd6db6fca10d9
Stanley <snicholl@gmail.com>
2018-02-01 10:11:25 -0800

    small seed change

--- db/seeds/03_prototype_reviews.js
+++ db/seeds/03_prototype_reviews.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex, Promise) {
   // Inserts sample users
   return knex('prototype_reviews').insert([
-    { id: 1, prototype_id: 1, user_id: 2, painGain: true, simple: true, changeRequest: 'I like the direction of where this is going. Needs to enable other communication methods beyond text.', nextSteps: 'Review with Brian Delmico' },
+    { id: 1, prototype_id: 1, user_id: 2, painGain: true, simple: false, changeRequest: 'I like the direction of where this is going. Needs to enable other communication methods beyond text.', nextSteps: 'Review with Brian Delmico' },
     { id: 2, prototype_id: 2, user_id: 3, painGain: true, simple: false, changeRequest: 'While it is simple and our users will love it, we need to enable sorting capabilities that can extend across the entire collection.', nextSteps: 'Make the updates and shoot me the new experience next week'}
   ])
   .then(() => {
